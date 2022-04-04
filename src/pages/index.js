fbd232943df1e64bea538e2b3aa63b0cbab18b20
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-04-04 11:10:22 +0100

    feat: initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+const Page = () => {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    const getTestFunction = async () => {
+      try {
+        const response = await fetch('/.netlify/functions/test-function');
+        const data = await response.json();
+        setResponse(data);
+      } catch (error) {
+        setResponse(error);
+      }
+    };
+
+    getTestFunction();
+  }, []);
+
+  return (
+    <main>
+      <h1>Index</h1>
+      {response ? <pre>{JSON.stringify(response, null, 2)}</pre> : null}
+    </main>
+  );
+};
+
+export default Page;
